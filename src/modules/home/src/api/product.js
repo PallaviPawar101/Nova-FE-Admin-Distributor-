f0ff9a17b69e15d883b0d82991159487258b18d2
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-05-10 15:14:24 +0530

    error-status-01

--- src/modules/home/src/api/product.js
+++ src/modules/home/src/api/product.js
@@ -15,15 +15,17 @@ export default{
         async handleGetData(){
             try{
                 console.log(process.env.VUE_APP_BASEURL,"api")
-                const data=await axios.get(`${process.env.VUE_APP_BASEURL}/products`);
+                const data=await axios.get(`${process.env.VUE_APP_BASEURL}/product`);
                 const res=await data.data;
                 this.productsdata=res;
             }catch(err){
-                this.handleError();
+                this.handleError("can't get the products, try after some time");
             }
         },
-        handleError(){
-            console.log(this.errorShow)
+        handleError(title){
+            this.errorShow.title=title
+            alert(this.errorShow.title)
+            this.errorShow.title=""
         }
     },
     async created(){
